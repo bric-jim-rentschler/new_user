908b53fa7b1730b0f5c8ea791b6fc0bc1d4874d7
Jim Rentschler <jrentschler123@gmail.com>
2018-02-12 00:07:49 -0500

    major changes

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,57 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, ValidationError
+from wtforms.validators import DataRequired, Length, Regexp, EqualTo
+from wtforms.fields.html5 import EmailField
+from validate_email import validate_email
+
+
+def validate_password(form, password):
+    password = str(password)
+    if not any(ch.isupper() for ch in password):
+        if not any(c.islower() for c in password):
+            if not any(n.isalnum() for n in password):
+                raise ValidationError('Please enter a password that has at least 7 characters including at least 1 '
+                                      'uppercase character, 1 lowercase character, and 1 special character or number.')
+
+def valid_email(form, email):
+    email = str(email)
+    if (validate_email(email, verify=True)):
+        raise ValidationError('This email address is already in use. If '
+                              'you’ve forgotten your password click here to retrieve it.')
+
 
 class CreateAccountForm(FlaskForm):
-    first_name = StringField('First Name', validators=[DataRequired()])
-    last_name = StringField('Last Name', validators=[DataRequired()])
-    email_address = StringField('Email Address', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
-    phone_number = StringField('Phone Number')
-    security_questions = SelectField('Security Question', choices=[(
-        'wsdylo', "What Street did you live on in 6th grade?"), ("test", "test")])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    first_name = StringField('First Name', validators=[DataRequired(message="First name is required"), Length(max=30)])
+    last_name = StringField('Last Name', validators=[DataRequired(message="Last name is required"), Length(max=30)])
+    email_address = EmailField('Email Address'
+                                , validators=[DataRequired(message="Email address is required"),
+                                              Length(max=100),
+                                              Regexp('[_\-a-zA-Z0-9\.\+]+@[a-zA-Z0-9](\.?[\-a-zA-Z0-9]*[a-zA-Z0-9])*',
+                                                     message="Please enter a valid email address."),
+                                              valid_email])
+    password = PasswordField('Password', validators=[DataRequired(message="Password is required"),
+                                                     Length(min=7),
+                                                     validate_password])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(message="Confirm password is required"),
+                                                 Length(min=7), EqualTo(fieldname='password'),
+                                                 validate_password])
+    phone_number = StringField('Phone Number',
+                                    validators=[DataRequired(message="Phone number is required"),
+                                                Length(max=17),
+                                                Regexp(r'(((\+[1-9]\d{0,2})[\-\s.](\d{1,3})|(((1\s)?)\(\d{3}\)))[\-\s.])'
+                                                        r'?(\d{1,8})([\-\s.](\d{1,8})){1,'r'4}(\sext\.\s\d{1,10})?')])
+    security_questions = SelectField('Security Question', choices=[
+        ("", "")
+        , ("wsdylo", "What Street did you live on in 6th grade?")
+        , ("wwycn", "What was your childhood nickname?")
+        , ("iwcdymafm", "In what city did your mother and father meet?")
+        , ("wwtlnoytgt", "What was the last name of your third grade teacher?")
+        , ("wwtnoyfsa", "What was the name of your first stuffed animal?")])
+    security_answers = PasswordField('Security Answer',
+                                     validators=[DataRequired(message="Security answers are required"),
+                                                 Length(max=100)])
+    sweepstakes = BooleanField('Yes, I would like to be enrolled in the Sweepstakes.', default="checked")
+    promotions = BooleanField('Yes, I would like to receive emails about special promotions or sales.',
+                              default="checked")
+    submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,19 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import CreateAccountForm
 
 @app.route('/')
-@app.route('/createaccount')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Home')
+
+@app.route('/createaccount', methods=['GET', 'POST'])
 def newuser():
     form = CreateAccountForm()
-    return render_template('create_account.html', title='Account Creation', form=form)
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}, e {}, p {}, cp {}, pn {}, sa {}'.format(
+            form.first_name.data, form.last_name.data, form.email_address.data
+            , form.password.data, form.confirm_password.data, form.phone_number.data
+            , form.security_answers.data))
+        return redirect('/index')
+    return render_template('create_account.html', title='Create Account', form=form)
